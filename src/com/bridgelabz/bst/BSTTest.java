04b1b1922ec1b3ab133e8b1a20deabcc3898385f
AshokKoda <a.koda59@gmail.com>
2022-04-06 10:15:02 +0530

    Added:UC2 - Create a binary tree and added to bst using size method

--- src/com/bridgelabz/bst/BSTTest.java
+++ src/com/bridgelabz/bst/BSTTest.java
@@ -16,4 +16,16 @@ public class BSTTest {
         System.out.println("Size is : " + myBinaryTree.getSize());
         Assert.assertEquals(4,myBinaryTree.getSize());
     }
+	
+	@Test
+    public void binaryTreeArrayTest() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		int numberArray[] = {56, 30, 70, 22, 40, 60, 95, 11, 65, 3, 16, 63, 67};
+		for (int number : numberArray) {
+            bst.add(number);
+        }
+		System.out.println(bst.root);
+		System.out.println("Size is : " + bst.getSize());
+		Assert.assertEquals(13, bst.getSize());
+	}
 }
